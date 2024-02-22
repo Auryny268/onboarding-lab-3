ace5b897863d14f1b6f133de83a10970d7e5f1e3
aay268 <aay268@nyu.edu>
2024-02-22 17:59:23 -0500

    Lab Complete

--- dv/exercise3.cpp
+++ dv/exercise3.cpp
@@ -2,29 +2,23 @@
 #include <cstdlib>
 #include <random>
 
-int main() {
-  VExercise3 model;
-}
-
 // Mystery1 -> Doesn't depend on clk cycles, so can use function instead
-uint16_t mystery1(uint16_t a, uint16_t b, uint16_t c) {
+uint8_t mystery1(uint8_t a, uint8_t b, uint8_t c) {
   // Assume I've passed in all bits correctly
   // Should return a 8-bit number
   switch (a) {
-    case 0:
-    return (model.b & 0x7) << 3 | (model.c & 0x7);
-    case 1:
-    return 1 << 6 | (model.c & 0x7) << 3 | (model.b & 0x7);
-    case 2:
-    return model.b & 0xFF;
-    case 3:
-    return model.c & 0xFF;
+    case 0: return ((b & 0x7) << 3) | (c & 0x7);
+    case 1: return (1 << 6) | ((c & 0x7) << 3) | (b & 0x7);
+    case 2: return b;
+    // (case 3):
+    default: return c;
   }
 }
 
 // Use Struct for Mystery2 to implement methods
-struct mystery2 {
-  uint16_t count;
+// -> Think Mystery2 "machine"
+struct Mystery2 {
+  uint16_t count = 0;
   uint16_t out;
 
   uint16_t reset(uint8_t a_in, uint8_t b_in) {
@@ -33,38 +27,74 @@ struct mystery2 {
     return out;
   }
 
-  // 0: out <= {a_in, out[7:0]};
-  // 1: out <= {out[15:8], b_in};
-  // 2: out <= {out[7:0], out[15:8]};
-  // 3: out <= {out[3:0], out[7:4], out[11:8], out[15:12]};
-  // 4: out <= {15'b0, ^out};
-  // count <= (count + 1) % 5;
-
   uint16_t step(uint8_t a_in, uint8_t b_in) {
     count %= 5;
     switch(count++) {
-      case 0:
-      out = a_in << 8 | (out & 0xFF);
+      case 0: out = a_in << 8 | (out & 0xFF);
       break;
-      case 1:
-      out = (out & 0xFF00) | b_in;
+      case 1: out = (out & 0xFF00) | b_in;
       break;
-      case 2:
-      out = out << 8 | out >> 8;
+      case 2: out = out << 8 | out >> 8;
       break;
-      case 3:
-      out = (out & 0xF) << 12 | (out & 0xF0) << 8 | (out & 0xF00) << 4 | (out & 0xF000);
+      case 3: out = (out << 12) | ((out & 0xF0) << 4) | ((out >> 4) & 0xF0) | (out >> 12);
       break;
-      case 4:
-      out = (^out) & 1;
-      break;
+      // (case 4): Reduction XOR - All bits XOR'ed with each other
+      default: out = (std::popcount(out)) & 1;
     }
     return out;
   }
+};
+
+struct Exercise3_Sim {
+  Mystery2 state;
+
+  uint16_t reset(uint8_t a, uint16_t b, uint16_t c) {
+    uint8_t a_in = mystery1(a & 0x3, b & 0xFF, c & 0xFF);
+    uint8_t b_in = mystery1(a >> 2, b >> 8, c >> 8);
+    return state.reset(a_in, b_in);
+  }
+
+  uint16_t step (uint8_t a, uint16_t b, uint16_t c) {
+    uint8_t a_in = mystery1(a & 0x3, b & 0xFF, c & 0xFF);
+    uint8_t b_in = mystery1(a >> 2, b >> 8, c >> 8);
+    return state.step(a_in, b_in);
+  }
+};
+
+void step(VExercise3& model) {
+  model.clk = 1;
+  model.eval();
+  model.clk = 0;
+  model.eval();
 }
 
+int main() {
+  VExercise3 model;
+  Exercise3_Sim sim;
+  int status = 0;
 
+  // Randomize a, b, and c
+  for (int num_test = 1; num_test < 100; ++num_test){
+    model.a = rand() & 0xF;
+    model.b = rand() & 0xFFFF;
+    model.c = rand() & 0xFFFF;
+    // Reset model first
+    model.reset = 1;
+    step(model);
+    if (model.out != sim.reset(model.a, model.b, model.c)) status = 1;
+    // Check for 100 cycles
 
+    model.reset = 0;
+    for (int cycles = 0; cycles < 100; ++cycles) {
+      uint16_t result = sim.step(model.a, model.b, model.c);
+      step(model);
+      if (model.out != result) status = 1;
+    }
+  }
+  if (status) std::cerr << "Failed test cases" << std::endl;
+  else std::cout << "Passed all test cases" << std::endl;
+  return status;
+}
 
 /*
 Test Mystery 1:
--- dv/exercise4.cpp
+++ dv/exercise4.cpp
@@ -1,5 +1,51 @@
 #include <VExercise4.h>
+#include <cstdlib>
+#include <random>
+
+
+// Just use rand numbers again
+bool test_sel(VExercise4& model) {
+  model.eval();
+  switch(model.sel) {
+    case 0: return (model.out != model.alpha);
+    break;
+    case 1: return (model.out != model.beta);
+    break;
+    case 2: return (model.out != model.gamma);
+    break;
+    default: return (model.out != (model.alpha & (model.beta | model.gamma)));
+  }
+}
 
 int main() {
   VExercise4 model;
+  int status = 0;
+  // Testing cs / "don't care" for sel
+  model.cs = 0;
+  do { if (model.out != 0) status = 1;
+  } while(++model.sel);
+  model.cs = 1;
+  // Testing various alpha, beta, gamma for all sel
+  for (int trials = 0; trials < 10000; ++trials) {
+    model.alpha = rand() & 0xFF;
+    model.beta = rand() & 0xFF;
+    model.gamma = rand() & 0xFF;
+    do {
+      model.eval();
+      switch(model.sel) {
+        case 0: if (model.out != model.alpha) status = 1;
+        break;
+        case 1: if (model.out != model.beta) status = 1;
+        break;
+        case 2: if (model.out != model.gamma) status = 1;
+        break;
+        default: if (model.out != (model.alpha & (model.beta | model.gamma))) status = 1;
+      }
+    } while(++model.sel);
+  }
+  if (status) std::cerr << "Failed test cases" << std::endl;
+  else std::cout << "Passed all test cases" << std::endl;
+  return status;
 }
+
+
